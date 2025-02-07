d27dd8dc618afb723c690f84a940c2eb55b5266b
ReggeMeister <mattiaswaern04@gmail.com>
2025-02-07 19:55:51 +0100

    tog bort onödiga kommentarer

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import "./styles/App.css"; // Se till att du har en CSS-fil för styling
+import "./styles/App.css"; 
 
 function App() {
   // Kolla om användaren har dark mode sparat i localStorage
